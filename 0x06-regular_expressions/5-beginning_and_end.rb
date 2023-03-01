738c18671089229622d373574370a75ebb2ae766
abuchijuliet <abuchijuliet44@gmail.com>
2023-03-01 22:32:23 +0000

    5-beginning_and_end.rb

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,1 +1,6 @@
-0-simply_match_school.rb
+#!/usr/bin/env ruby
+## This script accepts one argument and passes it to a regular expression,
+## matching method
+## The regular expression must be exactly matching a string that starts,
+## with h ends with n and can have any single character in between
+puts ARGV[0].scan(/^h.n$/).join
