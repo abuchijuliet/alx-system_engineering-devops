0b604e10d5fb748892599de0d746160b3d56e4bb
abuchijuliet <abuchijuliet44@gmail.com>
2023-03-01 22:13:02 +0000

    1-repetition_token_0.rb

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -2,4 +2,4 @@
 #This script accepts one argument and passes it to a regular expression,
 #matching method
 #The regular expression must match the given cases
-#puts ARGV[0].scan(/hbt{2,5}n/).join
+puts ARGV[0].scan(/hbt{2,5}n/).join
