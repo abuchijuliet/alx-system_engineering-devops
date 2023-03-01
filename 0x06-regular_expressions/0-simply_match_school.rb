9e0f3cd0c2671c24df8aa9bc8628f9b58a5132ad
abuchijuliet <abuchijuliet44@gmail.com>
2023-03-01 11:45:51 +0000

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -2,4 +2,5 @@
 #This script accepts one argument and passes it to a regular expression,
 #matching method
 #The regular expression must match School
-#puts ARGV[0].scan(/School/).join
+#
+##puts ARGV[0].scan(/School/).join
