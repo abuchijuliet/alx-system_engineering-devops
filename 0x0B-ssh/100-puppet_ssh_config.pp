8f760d70324bec90b665123e9bcab5522fc93458
abuchijuliet <abuchijuliet44@gmail.com>
2023-04-28 16:39:05 +0000

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,16 @@
-# make changes to config file using Puppet
-
+# Setting up my client config file
 include stdlib
 
-file_line { 'Refuse to authenticate using a password':
-ensure => present,
-path   => '/etc/ssh/ssh_config',
-line   => 'PasswordAuthentication no',
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'Use private key':
-ensure => present,
-path   => '/etc/ssh/ssh_config',
-line   => 'IdentityFile ~/.ssh/school'
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
--- 0x0B-ssh/README.md
+++ 0x0B-ssh/README.md
@@ -1,1 +1,2 @@
-0x0B. SSH
+# 0x0B. SSH
+Today's tasks involve setting up remote access into a server using SSH. Secure Shell (SSH) gives the user the ability to connect to a server remotely and execute bash type operations on it from their own terminal. SSH provides a secure way to do this.
